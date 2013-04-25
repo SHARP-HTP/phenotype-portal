fd9b9137c363a6abd3cba4bc1d67cdb69c05c86d
dsuesse <dsuesse@5e50eca5-c1b8-4ff5-ab9c-aaa7763a57bf>
2013-04-25 18:31:20 +0000

    Improves mime type detection.
    git-svn-id: http://svn.code.sf.net/p/sharpn/htp/code/trunk/HTP_client@162 5e50eca5-c1b8-4ff5-ab9c-aaa7763a57bf

--- src/edu/mayo/phenoportal/server/utils/MimeUtils.java
+++ src/edu/mayo/phenoportal/server/utils/MimeUtils.java
@@ -1,87 +1,53 @@
 package edu.mayo.phenoportal.server.utils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.logging.Logger;
 
-import edu.mayo.phenoportal.server.exception.PhenoportalFileException;
-import eu.medsea.mimeutil.MimeType;
-import eu.medsea.mimeutil.MimeUtil;
+import com.google.common.net.MediaType;
+import org.apache.tika.Tika;
 
 public class MimeUtils {
 
-    public static final String[] XML_MIMES = { "application/xml", "text/xml" };
-    public static final String[] HTML_MIMES = { "text/html" };
-    public static final String[] XLS_MIMES = { "application/x-msexcel", "application/vnd.ms-excel",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            "application/msword" };
-    public static final String[] ZIP_MIMES = { "application/zip", "application/x-zip",
-            "application/x-compressed" };
-    public static final String[] WORD_MIMES = { "application/msword",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" };
-    public static final String[] SAVE_MIMES = { "application/xml", "text/xml", "text/html",
-            "application/vnd.ms-excel", "application/x-msexcel",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/zip",
-            "application/x-zip", "application/x-compressed", "application/msword",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" };
+	/* http://www.iana.org/assignments/media-types */
+	public static final String[] XML_MIMES = { "application/xml", "text/xml" };
+	public static final String[] HTML_MIMES = { "text/html" };
+	public static final String[] XLS_MIMES = { "application/vnd.ms-excel",
+	                                           "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" };
+	public static final String[] ZIP_MIMES = { "application/zip" };
+	public static final String[] WORD_MIMES = { "application/msword",
+	                                            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" };
+	public static final String[] PDF_MIMES = { "application/pdf" };
 
-    private static Logger logger = Logger.getLogger(MimeUtils.class.getName());
+	public static String getMimeType(File file) throws IOException {
+		Tika tika = new Tika();
+		String type = tika.detect(file);
+		MediaType mediaType = MediaType.parse(type);
+		return mediaType.toString();
+	}
 
-    public static String getMimeType(File file) throws PhenoportalFileException {
-        String mime;
-        Collection mimeTypes = MimeUtil.getMimeTypes(file);
-        MimeType mimeType = MimeUtil.getMostSpecificMimeType(mimeTypes);
-        mime = mimeType.toString();
-        return mime;
-    }
+	public static boolean isXmlFile(String mime) {
+		return Arrays.asList(XML_MIMES).contains(mime);
+	}
 
-    public static boolean isXmlFile(File file) throws PhenoportalFileException {
-        return Arrays.asList(XML_MIMES).contains(getMimeType(file));
-    }
+	public static boolean isXlsFile(String mime) {
+		return Arrays.asList(XLS_MIMES).contains(mime);
+	}
 
-    public static boolean isXlsFile(File file) throws PhenoportalFileException {
-        return Arrays.asList(XLS_MIMES).contains(getMimeType(file));
-    }
+	public static boolean isHtmlFile(String mime) {
+		return Arrays.asList(HTML_MIMES).contains(mime);
+	}
 
-    public static boolean isHtmlFile(File file) throws PhenoportalFileException {
-        return Arrays.asList(HTML_MIMES).contains(getMimeType(file));
-    }
+	public static boolean isZipFile(String mime) {
+		return Arrays.asList(ZIP_MIMES).contains(mime);
+	}
 
-    public static boolean isZipFile(File file) throws PhenoportalFileException {
-        return Arrays.asList(ZIP_MIMES).contains(getMimeType(file));
-    }
+	public static boolean isWordFile(String mime) {
+		return Arrays.asList(WORD_MIMES).contains(mime);
+	}
 
-    public static boolean isWordFile(File file) throws PhenoportalFileException {
-        return Arrays.asList(WORD_MIMES).contains(getMimeType(file));
-    }
-
-    public static boolean isSaveFile(File file) throws PhenoportalFileException {
-        return Arrays.asList(SAVE_MIMES).contains(getMimeType(file));
-    }
-
-    public static boolean isXmlFile(String mime) {
-        return Arrays.asList(XML_MIMES).contains(mime);
-    }
-
-    public static boolean isXlsFile(String mime) {
-        return Arrays.asList(XLS_MIMES).contains(mime);
-    }
-
-    public static boolean isHtmlFile(String mime) {
-        return Arrays.asList(HTML_MIMES).contains(mime);
-    }
-
-    public static boolean isZipFile(String mime) {
-        return Arrays.asList(ZIP_MIMES).contains(mime);
-    }
-
-    public static boolean isWordFile(String mime) {
-        return Arrays.asList(WORD_MIMES).contains(mime);
-    }
-
-    public static boolean isSaveFile(String mime) {
-        return Arrays.asList(SAVE_MIMES).contains(mime);
-    }
+	public static boolean isPdfFile(String mime) {
+		return Arrays.asList(PDF_MIMES).contains(mime);
+	}
 
 }
